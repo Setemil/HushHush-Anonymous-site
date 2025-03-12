19bb58263d0bb6fa76a2162fbe700d11ab5bb4d1
Loye Setemi <setemiloye@gmail.com>
2025-03-12 15:46:39 +0100

    Force add full vendor folder

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b729bf57300bfbf5a12ccfb02bee22ef1729a5e1',
+        'reference' => 'c5679a36910eb0bb9517325f4900f46f4e826eca',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b729bf57300bfbf5a12ccfb02bee22ef1729a5e1',
+            'reference' => 'c5679a36910eb0bb9517325f4900f46f4e826eca',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
