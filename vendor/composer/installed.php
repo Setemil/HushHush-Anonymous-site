c5679a36910eb0bb9517325f4900f46f4e826eca
Loye Setemi <setemiloye@gmail.com>
2025-03-12 15:40:15 +0100

    add vendor folder for deployment

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e4853a05cc5a87e9dfdc2d4e0d482ab87aca5570',
+        'reference' => 'b729bf57300bfbf5a12ccfb02bee22ef1729a5e1',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e4853a05cc5a87e9dfdc2d4e0d482ab87aca5570',
+            'reference' => 'b729bf57300bfbf5a12ccfb02bee22ef1729a5e1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
