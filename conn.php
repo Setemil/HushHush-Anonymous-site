ed7d79ac8b8a5c7610b8c6965d4d410febb9f2d2
Loye Setemi <setemiloye@gmail.com>
2025-03-12 14:13:39 +0100

    Commit made to fix issues with deployment

--- conn.php
+++ conn.php
@@ -1,11 +1,20 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "hushhush_db";
+require 'vendor/autoload.php';
 
+// Load environment variables from .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Read database connection details from environment variables
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASSWORD'];
+$db = $_ENV['DB_NAME'];
+
+// Create connection
 $conn = mysqli_connect($host, $user, $pass, $db);
 
+// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
